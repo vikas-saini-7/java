deafcfeab350ea00d4f1d0a8334e3945f4b6c466
vikas-saini-7 <vikas289888@gmail.com>
2023-10-20 18:54:08 +0530

    Changes in file

--- Backtracking.java
+++ Backtracking.java
@@ -15,8 +15,22 @@ public class Backtracking {
     }
         // String str = "ABC";
         // printPermutation(str, "", 0);
+    
+    // public static void printPermutation(String str, String perm, int idx){
+    //     if(str.length() == 0){
+    //         System.out.println(perm);
+    //         return;
+    //     }
+
+    //     for(int i=0; i<str.length(); i++){
+    //         char currChar = str.charAt(i);
+    //         String newStr = str.substring(0, i) + str.substring(i+1);
+    //         printPermutation(newStr ,  perm + currChar, idx);
+    //     }
+    // }
 
     // N -> Queens 
+
     public static void main(String args[]){
 
     }
